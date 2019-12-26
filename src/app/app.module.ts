4fd46f657866610804210194271ddc9260e81d4f
kodurivamshi <vkoduri555@gmail.com>
2019-12-26 16:37:23 +0530

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LinkComponent } from './link/link.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    LinkComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
